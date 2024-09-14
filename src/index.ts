ab39d92223dc002bdd8ffed6484a154782b3ee4b
SukkaW <isukkaw@gmail.com>
2024-09-14 15:08:14 +0800

    feat: add configuration menu

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,21 @@ import { renderConfigUI } from './ui';
 (() => {
   spoofLocation();
 
+  if ('registerMenuCommand' in GM && typeof GM.registerMenuCommand === 'function') {
+    GM.registerMenuCommand(
+      'Configuration',
+      () => {
+        const a = document.createElement('a');
+        a.href = 'https://location-guard-ng.skk.moe/options';
+        a.target = '_blank';
+        a.style.display = 'none';
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+      }
+    );
+  }
+
   if (
     window.location.host === 'localhost:3000'
     || window.location.host === 'location-guard-ng.skk.moe'
