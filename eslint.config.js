24e038b63d69b75dd8c73e912b9d776a7d7825d3
SukkaW <isukkaw@gmail.com>
2024-09-15 21:49:28 +0800

    chore: use monorepo

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,13 @@
 'use strict';
 
-module.exports = require('eslint-config-sukka').sukka({
-  ts: {
-    // TODO: use projectService with multiplt tsconfig.json
-    tsconfigPath: [
-      'tsconfig.json',
-      'tsconfig.rollup.config.json'
-    ]
+module.exports = require('eslint-config-sukka').sukka(
+  {},
+  {
+    files: [
+      'packages/userscripts/src/dummy.js'
+    ],
+    rules: {
+      'sukka/unicorn/no-empty-file': 'off'
+    }
   }
-}, {
-  files: ['src/dummy.js'],
-  rules: {
-    'sukka/unicorn/no-empty-file': 'off'
-  }
-});
+);
