2169e503f4f3c3cb970c8afbe52f0da0d799df57
SukkaW <isukkaw@gmail.com>
2024-09-30 03:49:48 +0800

    chore: housekeeping

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,14 @@
 'use strict';
 
 module.exports = require('eslint-config-sukka').sukka(
-  {},
+  {
+    react: {
+      enable: true,
+      files: [
+        'packages/web/**/*.{js,jsx,ts,tsx}'
+      ]
+    }
+  },
   {
     files: [
       'packages/userscripts/src/dummy.js'
--- packages/web/src/app/theme-registry.tsx
+++ packages/web/src/app/theme-registry.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import createCache, { type Options as EmotionCacheOptions } from '@emotion/cache';
+import createCache from '@emotion/cache';
+import type { Options as EmotionCacheOptions } from '@emotion/cache';
 import { useServerInsertedHTML } from 'next/navigation';
 import { CacheProvider } from '@emotion/react';
 import { CssVarsProvider } from '@mui/joy/styles';
